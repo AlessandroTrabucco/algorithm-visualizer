f627bb914c1dc28f5917d6ecea7bc54c4f1d1c50
Alessandro Trabucco <113837087+AlessandroTrabucco@users.noreply.github.com>
2022-09-18 17:04:08 +0200

    Delete algorithm_visualizer directory

--- algorithm_visualizer/sorting_visualizer/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const randomBetween = (a, b) => {
-  return Math.floor(Math.random() * b + a);
-};
-
-export const sleep = ms => {
-  return new Promise(resolve => setTimeout(resolve, ms));
-};
